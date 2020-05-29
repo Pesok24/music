1dda70f7b851370f6f5431dc05f11b59d9112190
Pesok24 <ipes97@mail.ru>
2020-05-29 14:20:11 +0300

    Release 0

---
+++ index.js
@@ -0,0 +1,12 @@
+let music = require('musicmatch')({apikey:"52d6ace43232f2a871e699dc324ffbe8"});
+
+
+music.trackSearch({q:"Любимка", page:1, page_size:3})
+    .then(function(data){
+        console.log(data.message.body.track_list);
+    }).catch(function(err){
+        console.log(err);
+})
+
+
+
